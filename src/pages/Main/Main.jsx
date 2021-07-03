17e633f0d761ed1c689021bad97e4a5176388139
Gabriel Merigo <gabriel.merigo.dev@gmail.com>
2021-07-03 14:28:25 -0300

    new atts

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,11 +1,46 @@
-import React, { useState } from 'react';
-import { FaGithub, FaPlus } from 'react-icons/fa'
-import { Container, Form, SubmitButton } from './Styles'
+import React, { useState, useCallback } from 'react';
+import { FaGithub, FaPlus, FaSpinner, FaBars, FaTrash } from 'react-icons/fa'
+import { Container, Form, SubmitButton, List, DeleteButton } from './Styles'
 
-export default function Main(){
+import api from '../../services/api'
+
+export default function Main() {
   const [newRepo, setNewRepo] = useState('')
+  const [repositorios, setRepositorios] = useState([])
+  const [loading, setLoading] = useState(false)
 
-  return(
+
+  async function handleSubmit() {
+    setLoading(true)
+    try {
+
+      if(newRepo === ''){
+        throw new Error('Você precisa indicar um repositório.')
+      }
+
+      const response = await api.get(`repos/${newRepo}`)
+
+      const hasRepo = repositorios.find(repo => repo.name === newRepo)
+
+      const data = {
+        name: response.data.full_name
+      }
+      console.log(data)
+      setRepositorios([...repositorios, data])
+      setNewRepo('')
+    } catch (err) {
+      console.log(err)
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  const handleDelete = useCallback(repo => {
+    const find = repositorios.filter(r => r.name !== repo);
+    setRepositorios(find);
+  }, [repositorios])
+
+  return (
     <Container>
 
       <h1>
@@ -13,13 +48,38 @@ export default function Main(){
         Meus Repositórios
       </h1>
 
-      <Form onSubmit={() => {}}>
-        <input type="text" placeholder="Adicionar Repos" value={newRepo} onChange={e => setNewRepo(e.target.value)}/>
+      <Form>
+        <input
+          type="text"
+          placeholder="Adicionar Repos"
+          value={newRepo}
+          onChange={e => setNewRepo(e.target.value)}
+        />
 
-        <SubmitButton>
-          <FaPlus color="#fff" size={14}/>
+        <SubmitButton onClick={handleSubmit} loading={loading ? 1 : 0}>
+          {loading ? (
+            <FaSpinner color="#fff" size={14} />
+          ) : (
+            <FaPlus color="#fff" size={14} />
+          )}
         </SubmitButton>
       </Form>
+
+      <List>
+        {repositorios.map(repo => (
+          <li key={repo.name} >
+            <span>
+              <DeleteButton onClick={() => handleDelete(repo.name)} >
+                <FaTrash size={14} />
+              </DeleteButton>
+              {repo.name}
+            </span>
+            <a href="">
+              <FaBars size={20} />
+            </a>
+          </li>
+        ))}
+      </List>
 
     </Container>
   )
--- src/pages/Main/Styles.jsx
+++ src/pages/Main/Styles.jsx
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, { keyframes, css } from 'styled-components';
 
 export const Container = styled.div`
   max-width: 700px;
@@ -20,6 +20,16 @@ export const Container = styled.div`
   }
 `;
 
+const animate = keyframes`
+  from{
+    transform: rotate(0deg);
+  }
+
+  to{
+    transform:rotate(360deg);
+  }
+`
+
 export const Form = styled.form`
   margin-top: 30px;
   display: flex;
@@ -34,7 +44,10 @@ export const Form = styled.form`
   }
 `;
 
-export const SubmitButton = styled.div`
+export const SubmitButton = styled.div.attrs(props => ({
+  type: 'submit',
+  disabled: props.loading
+}))`
   background: #0d2636;
   border: 0;
   border-radius: 4px;
@@ -43,4 +56,53 @@ export const SubmitButton = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+
+  &[disabled]{
+    cursor:not-allowed;
+    opacity: 0.5;
+  }
+
+  ${props => props.loading &&
+    css`
+      svg{
+        animation: ${animate} 2s linear infinite;
+      }  
+    
+    `
+  }
+`;
+
+export const List = styled.ul`
+  list-style:none;
+  margin-top: 20px;
+
+  li{
+    padding:15px 0;
+    display:flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+    
+    & + li{
+      border-top:1px solid #c0c0c0;
+    }
+
+    a{
+      color:#0d2636;
+      text-decoration: none;
+    }
+
+  }
+`;
+
+export const DeleteButton = styled.button.attrs({
+  type:'button'
+})`
+  margin-left:6px;
+  background: transparent;
+  color:#0d2636;
+  border:0;
+  padding: 8px 7px;
+  outline:0;
+  border-radius:4px;
 `;
