f9575f01b7db4d2bc30c2bdad98a47822393d00f
Gabriel Merigo <gabriel.merigo.dev@gmail.com>
2021-07-04 01:07:47 -0300

    mais um projeto com react finalizado!

--- src/pages/Repositorio/Repositorio.jsx
+++ src/pages/Repositorio/Repositorio.jsx
@@ -1,38 +1,120 @@
 import React, { useState, useEffect } from 'react';
-import { Container } from './StylesRepo';
+import { Container, Owner, Loading, BackButton, IssuesList, PageActions } from './StylesRepo.jsx';
+import { FaArrowLeft } from 'react-icons/fa'
 import api from '../../services/api';
 
-export default function Repositorio({match}){
+export default function Repositorio({ match }) {
 
   const [repositorio, setRepositorio] = useState({})
   const [issues, setIssues] = useState([])
   const [loading, setLoading] = useState(true)
+  const [page, setPage] = useState(1)
+  const [status, setStatus] = useState()
 
   useEffect(() => {
-    async function load(){
+    async function load() {
       const nomeRepo = decodeURIComponent(match.params.repositorio)
 
       const [repositorioData, issuesData] = await Promise.all([
         api.get(`/repos/${nomeRepo}`),
         api.get(`/repos/${nomeRepo}/issues`, {
           params: {
-            state: 'open',
+            state: status,
             per_page: 5
           }
         })
       ])
-      setRepositorio(repositorio.data)
+      setRepositorio(repositorioData.data)
       setIssues(issuesData.data)
       setLoading(false)
     }
 
     load();
 
-  }, [])
+  }, [match.params.repositorio, status])
 
-  return(
-    <Container style={{color: '#fff'}}>
+  useEffect(() => {
+    async function loadIssue() {
+      const nomeRepo = decodeURIComponent(match.params.repositorio)
 
+      const response = await api.get(`/repos/${nomeRepo}/issues`, {
+        params: {
+          state: status,
+          page,
+          per_page: 5
+        }
+      })
+
+      setIssues(response.data)
+
+    }
+
+    loadIssue()
+
+  }, [match.params.repositorio, page, status]);
+
+  const filterIssues = e => { 
+    e.preventDefault()
+    setStatus(e.target.textContent)
+  }
+
+  function handlePage(action) {
+    setPage(action === 'back' ? page - 1 : page + 1)
+  }
+
+  if (loading) {
+    return (
+      <Loading>
+        <h1>Carregando...</h1>
+      </Loading>
+    )
+  }
+
+  return (
+    <Container>
+      <BackButton to="/">
+        <FaArrowLeft color="#000" size={30} />
+      </BackButton>
+
+      <Owner>
+        <img src={repositorio.owner.avatar_url} alt={repositorio.owner.login} />
+        <h1>{repositorio.name}</h1>
+        <p>{repositorio.description}</p>
+      </Owner>
+
+      <IssuesList>
+        <div className="btns">
+          <button onClick={filterIssues}>closed</button>
+          <button onClick={filterIssues}>open</button>
+          <button onClick={filterIssues}>all</button>
+        </div>
+        {issues.map(issue => (
+          <li key={String(issue.id)}>
+            <img src={issue.user.avatar_url} alt={issue.user.login} />
+            <div>
+              <strong>
+                <a href={issue.html_url}>{issue.title}</a>
+
+                {issue.labels.map(label => {
+                  <span key={String(label.id)}>{label.name}</span>
+                })}
+              </strong>
+
+              <p>{issue.user.login}</p>
+            </div>
+          </li>
+        ))}
+      </IssuesList>
+
+      <PageActions>
+        <button type="button" onClick={() => handlePage('back')} disabled={page < 2}>
+          Voltar
+        </button>
+
+        <button type="button" onClick={() => handlePage('next')}>
+          Próxima
+        </button>
+      </PageActions>
     </Container>
   )
 }
